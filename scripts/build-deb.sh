946adf92362a2d68c7816811a23773b1680a0bdf
Uri Herrera <hello@uriherrera.com>
2025-06-11 17:04:21 -0600

    initial commit

---
+++ scripts/build-deb.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+
+#############################################################################################################################################################################
+#   The license used for this file and its contents is: BSD-3-Clause                                                                                                        #
+#                                                                                                                                                                           #
+#   Copyright <2025> <Uri Herrera <uri_herrera@nxos.org>>                                                                                                                   #
+#                                                                                                                                                                           #
+#   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:                          #
+#                                                                                                                                                                           #
+#    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.                                        #
+#                                                                                                                                                                           #
+#    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer                                      #
+#       in the documentation and/or other materials provided with the distribution.                                                                                         #
+#                                                                                                                                                                           #
+#    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software                    #
+#       without specific prior written permission.                                                                                                                          #
+#                                                                                                                                                                           #
+#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,                      #
+#    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS                  #
+#    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE                 #
+#    GOODS OR SERVICES; LOSS OF USE, DATA,   OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,                      #
+#    STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   #
+#############################################################################################################################################################################
+
+
+# -- Exit on errors.
+
+set -e
+
+
+# -- Download Source.
+
+SRC_DIR="$(mktemp -d)"
+
+git clone --depth 1 --branch "$DFL_UTILS_BRANCH" https://gitlab.com/desktop-frameworks/utils.git "$SRC_DIR/utils-src"
+
+cd "$SRC_DIR/utils-src"
+
+
+# -- Configure Build.
+
+meson setup .build --prefix=/usr --buildtype=release
+
+
+# -- Compile Source.
+
+ninja -C .build -k 0 -j "$(nproc)"
+
+
+# -- Create a temporary DESTDIR.
+
+DESTDIR="$(pwd)/pkg"
+
+rm -rf "$DESTDIR"
+
+
+# -- Install to DESTDIR.
+
+DESTDIR="$DESTDIR" ninja -C .build install
+
+
+# -- Create DEBIAN control file.
+
+mkdir -p "$DESTDIR/DEBIAN"
+
+PKGNAME="dfl-utils"
+MAINTAINER="uri_herrera@nxos.org"
+ARCHITECTURE="$(dpkg --print-architecture)"
+DESCRIPTION="This library provides a thin wrapper around QApplication, QGuiApplication and QCoreApplication, to provide single-instance functionality."
+
+cat > "$DESTDIR/DEBIAN/control" <<EOF
+Package: $PKGNAME
+Version: $PACKAGE_VERSION
+Section: utils
+Priority: optional
+Architecture: $ARCHITECTURE
+Maintainer: $MAINTAINER
+Description: $DESCRIPTION
+EOF
+
+
+# -- Build the Debian package.
+
+cd "$(dirname "$DESTDIR")"
+
+dpkg-deb --build "$(basename "$DESTDIR")" "${PKGNAME}_${PACKAGE_VERSION}_${ARCHITECTURE}.deb"
+
+
+# -- Move .deb to ./build/ for CI consistency.
+
+mkdir -p "$GITHUB_WORKSPACE/build"
+
+mv "${PKGNAME}_${PACKAGE_VERSION}_${ARCHITECTURE}.deb" "$GITHUB_WORKSPACE/build/"
+
+echo "Debian package created: $(pwd)/build/${PKGNAME}_${PACKAGE_VERSION}_${ARCHITECTURE}.deb"
